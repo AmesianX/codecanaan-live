e6b102e9b90b23261d2f96b56257d24104d8d4ae
spooky <spooky@spooky-VirtualBox.(none)>
2013-01-24 23:35:42 +0800

    language zh-TW

--- install.sh
+++ install.sh
@@ -3,10 +3,9 @@
 mount -t proc none /proc/
 mount -t sysfs none /sys/
 export HOME=/root
-export LC_ALL=C
 
-locale-gen en
-update-locale LANG=en LANGUAGE=en LC_ALL=en
+locale-gen zh_TW.UTF-8
+update-locale LANG=zh_TW.UTF-8
 
 
 
@@ -14,6 +13,7 @@ sudo apt-get update
 
 sudo apt-get --yes install fontconfig fontconfig-config libasound2 libatk1.0-0 libatk1.0-data libavahi-client3 libavahi-common-data libavahi-common3 libcairo2 libcups2 libdatrie1 libdrm-nouveau2 libfm-data libfm-gtk-bin libfm-gtk-data libfm-gtk3 libfm3 libfontconfig1 libfontenc1 libgdk-pixbuf2.0-0 libgdk-pixbuf2.0-common libgee2 libgif4 libgtk2.0-0 libgtk2.0-common libice6 libid3tag0 libimlib2 libiw30 libjasper1 libjbig0 libjpeg-turbo8 libjpeg8 libllvm3.1 libmenu-cache1 libmtdev1 libobrender27 libobt0 libpango1.0-0 libpixman-1-0 libpolkit-agent-1-0 libsm6 libstartup-notification0 libthai-data libthai0 libtiff5 libwnck-common libwnck22 libx11-xcb1 libxatracker1 libxaw7 libxcb-dri2-0 libxcb-render0 libxcb-shm0 libxcb-util0 libxcomposite1 libxcursor1 libxdamage1 libxfixes3 libxfont1 libxft2 libxi6 libxinerama1 libxkbfile1 libxml2 libxmu6 libxpm4 libxrandr2 libxrender1 libxres1 libxt6 libxv1 libxvmc1 lxde-common lxde-core lxmenu-data lxpanel lxsession lxsession-data openbox pcmanfm shared-mime-info ttf-dejavu-core x11-common x11-xkb-utils xserver-common xserver-xorg xserver-xorg-core xserver-xorg-input-all xserver-xorg-input-evdev xserver-xorg-input-mouse xserver-xorg-input-synaptics xserver-xorg-input-vmmouse xserver-xorg-input-wacom xserver-xorg-video-all xserver-xorg-video-ati xserver-xorg-video-cirrus xserver-xorg-video-fbdev xserver-xorg-video-intel xserver-xorg-video-mach64 xserver-xorg-video-mga xserver-xorg-video-modesetting xserver-xorg-video-neomagic xserver-xorg-video-nouveau xserver-xorg-video-openchrome xserver-xorg-video-qxl xserver-xorg-video-r128 xserver-xorg-video-radeon xserver-xorg-video-s3 xserver-xorg-video-savage xserver-xorg-video-siliconmotion xserver-xorg-video-sis xserver-xorg-video-sisusb xserver-xorg-video-tdfx xserver-xorg-video-trident xserver-xorg-video-vesa xserver-xorg-video-vmware
 
+sudo apt-get --yes install language-support-zh language-pack-gnome-zh* ttf-arphic-*
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb
 
